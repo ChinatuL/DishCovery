834cffd9af9b294cf5505033f6190dd0fa05538f
Chinatu Lucia Eke <chinatuluciaeke@gmail.com>
2023-12-28 13:00:49 +0100

    complete terms of use page

--- src/pages/TermsOfUse.jsx
+++ src/pages/TermsOfUse.jsx
@@ -1,6 +1,286 @@
+import { Link } from "react-router-dom";
+import SectionTitle from "../components/SectionTitle";
+import { FaArrowLeftLong } from "react-icons/fa6";
 const TermsOfUse = () => {
-  return (
-    <div>TermsOfUse</div>
-  )
-}
-export default TermsOfUse
+    return (
+        <div className='flex flex-col justify-center items-center gap-8 mx-auto px-4 lg:px-0 py-8 text-xl lg:w-8/12'>
+            <Link
+                to='/'
+                className='flex items-center gap-2 border border-zinc-950 w-fit rounded-md px-4 py-2 transition duration-300 hover:bg-zinc-950 hover:text-slate-50'
+            >
+                <FaArrowLeftLong className='inline-block' />{" "}
+                <span className='text-xl'>Back to home</span>
+            </Link>
+            <div className='self-center leading-8'>
+                <h1 className='text-center text-4xl font-semibold'>
+                    Terms Of Use
+                </h1>
+                <p className='text-center text-2xl py-4'>
+                    Effective Date: December 01, 2023
+                </p>
+                <p className='uppercase'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Delectus adipisci assumenda earum tenetur molestiae
+                    consequatur fugiat rem laudantium illo praesentium ipsa
+                    illum placeat, deleniti cumque quidem necessitatibus
+                    aspernatur quas officiis amet officia vitae error velit.
+                    Nesciunt rerum corrupti est iusto quos, dignissimos numquam
+                    omnis blanditiis excepturi dolorum vero saepe illum. Fuga
+                    doloremque perspiciatis modi laborum, distinctio tempora
+                    aliquam obcaecati sunt sed, nihil quaerat voluptate neque
+                    cumque eveniet. Mollitia quae expedita rerum optio fugiat
+                    tempore vitae, nulla ab? Eaque, numquam dolorem soluta vel
+                    sequi voluptatem eveniet sint nostrum dolorum repudiandae
+                    cum ab? Doloremque debitis fugit, veritatis sed quas
+                    deserunt fugiat nam.
+                </p>
+            </div>
+            <section className='flex flex-col gap-4'>
+                <SectionTitle size='text-3xl' text='Highlights' />
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quasi adipisci veritatis ex tempore omnis odio quo enim
+                    possimus eveniet ducimus fugit libero, est voluptatem
+                    tenetur?
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quasi adipisci veritatis ex tempore omnis odio quo enim
+                    possimus eveniet ducimus fugit libero, est voluptatem
+                    tenetur?
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quasi adipisci veritatis ex tempore omnis odio quo enim
+                    possimus eveniet ducimus fugit libero, est voluptatem
+                    tenetur?
+                </p>
+            </section>
+            <section className='flex flex-col gap-4'>
+                <SectionTitle
+                    size='text-3xl'
+                    text='Application and Acceptance'
+                />
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quasi adipisci veritatis ex tempore omnis odio quo enim
+                    possimus eveniet ducimus fugit libero, est voluptatem
+                    tenetur?
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quasi adipisci veritatis ex tempore omnis odio quo enim
+                    possimus eveniet ducimus fugit libero, est voluptatem
+                    tenetur?
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quasi adipisci veritatis ex tempore omnis odio quo enim
+                    possimus eveniet ducimus fugit libero, est voluptatem
+                    tenetur?
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quasi adipisci veritatis ex tempore omnis odio quo enim
+                    possimus eveniet ducimus fugit libero, est voluptatem
+                    tenetur?
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quasi adipisci veritatis ex tempore omnis odio quo enim
+                    possimus eveniet ducimus fugit libero, est voluptatem
+                    tenetur?
+                </p>
+            </section>
+            <section className='flex flex-col gap-4'>
+                <SectionTitle
+                    size='text-3xl'
+                    text='Info Automatically Collected'
+                />
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Quod, consectetur accusantium ullam nulla reprehenderit odio
+                    nobis quam quisquam velit! Et vel neque natus hic! Modi
+                    corrupti quasi, saepe numquam tenetur explicabo sed optio
+                    cupiditate aspernatur soluta excepturi officia quibusdam
+                    commodi perspiciatis expedita ducimus veritatis amet sint,
+                    libero unde quia aliquam!
+                </p>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Quod, consectetur accusantium ullam nulla reprehenderit odio
+                    nobis quam quisquam velit! Et vel neque natus hic! Modi
+                    corrupti quasi, saepe numquam tenetur explicabo sed optio
+                    cupiditate aspernatur soluta excepturi officia quibusdam
+                    commodi perspiciatis expedita ducimus veritatis amet sint,
+                    libero unde quia aliquam!
+                </p>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Quod, consectetur accusantium ullam nulla reprehenderit odio
+                    nobis quam quisquam velit! Et vel neque natus hic! Modi
+                    corrupti quasi, saepe numquam tenetur explicabo sed optio
+                    cupiditate aspernatur soluta excepturi officia quibusdam
+                    commodi perspiciatis expedita ducimus veritatis amet sint,
+                    libero unde quia aliquam!
+                </p>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Quod, consectetur accusantium ullam nulla reprehenderit odio
+                    nobis quam quisquam velit! Et vel neque natus hic! Modi
+                    corrupti quasi, saepe numquam tenetur explicabo sed optio
+                    cupiditate aspernatur soluta excepturi officia quibusdam
+                    commodi perspiciatis expedita ducimus veritatis amet sint,
+                    libero unde quia aliquam!
+                </p>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Quod, consectetur accusantium ullam nulla reprehenderit odio
+                    nobis quam quisquam velit! Et vel neque natus hic! Modi
+                    corrupti quasi, saepe numquam tenetur explicabo sed optio
+                    cupiditate aspernatur soluta excepturi officia quibusdam
+                    commodi perspiciatis expedita ducimus veritatis amet sint,
+                    libero unde quia aliquam!
+                </p>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Quod, consectetur accusantium ullam nulla reprehenderit odio
+                    nobis quam quisquam velit! Et vel neque natus hic! Modi
+                    corrupti quasi, saepe numquam tenetur explicabo sed optio
+                    cupiditate aspernatur soluta excepturi officia quibusdam
+                    commodi perspiciatis expedita ducimus veritatis amet sint,
+                    libero unde quia aliquam!
+                </p>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Quod, consectetur accusantium ullam nulla reprehenderit odio
+                    nobis quam quisquam velit! Et vel neque natus hic! Modi
+                    corrupti quasi, saepe numquam tenetur explicabo sed optio
+                    cupiditate aspernatur soluta excepturi officia quibusdam
+                    commodi perspiciatis expedita ducimus veritatis amet sint,
+                    libero unde quia aliquam!
+                </p>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Quod, consectetur accusantium ullam nulla reprehenderit odio
+                    nobis quam quisquam velit! Et vel neque natus hic! Modi
+                    corrupti quasi, saepe numquam tenetur explicabo sed optio
+                    cupiditate aspernatur soluta excepturi officia quibusdam
+                    commodi perspiciatis expedita ducimus veritatis amet sint,
+                    libero unde quia aliquam!
+                </p>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Quod, consectetur accusantium ullam nulla reprehenderit odio
+                    nobis quam quisquam velit! Et vel neque natus hic! Modi
+                    corrupti quasi, saepe numquam tenetur explicabo sed optio
+                    cupiditate aspernatur soluta excepturi officia quibusdam
+                    commodi perspiciatis expedita ducimus veritatis amet sint,
+                    libero unde quia aliquam!
+                </p>
+            </section>
+            <section className='flex flex-col gap-4'>
+                <SectionTitle size='text-3xl' text='How do we use info?' />
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Eveniet, tempora corporis consequatur eaque excepturi, iure
+                    ipsa dolores ullam dolor facilis dignissimos molestiae quos
+                    sequi corrupti unde velit numquam dolore cum est officiis,
+                    ea fugiat! Qui perspiciatis veritatis possimus doloremque,
+                    mollitia corporis tenetur. Voluptatum voluptatem, iusto
+                    aliquam repellendus optio molestias voluptates veritatis.
+                    Ratione atque molestias accusamus voluptatibus, eum cum,
+                    corrupti dicta rerum nesciunt pariatur debitis voluptatum
+                    dignissimos distinctio perspiciatis animi suscipit. Saepe
+                    nulla quis illum, eius aut, unde deleniti facere iure
+                    possimus dolores ducimus sint debitis quibusdam rerum
+                    accusamus velit dolor impedit nobis officiis corrupti
+                    quisquam modi laboriosam fugit? Molestiae quas ipsam esse
+                    repudiandae adipisci soluta, eum ea odio quos numquam?
+                    Officia quasi similique quae fugiat accusantium porro!
+                    Nostrum, similique. Quisquam velit placeat, cumque sequi
+                    tempore debitis ipsam voluptates, recusandae itaque sapiente
+                    eligendi totam nemo cupiditate nostrum? Nam neque dolores
+                    assumenda magnam doloremque non similique, laborum autem,
+                    porro ullam beatae qui?
+                </p>
+            </section>
+            <section className='flex flex-col gap-4'>
+                <SectionTitle size='text-3xl' text='How do we share info?' />
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Eveniet, tempora corporis consequatur eaque excepturi, iure
+                    ipsa dolores ullam dolor facilis dignissimos molestiae quos
+                    sequi corrupti unde velit numquam dolore cum est officiis,
+                    ea fugiat! Qui perspiciatis veritatis possimus doloremque,
+                    mollitia corporis tenetur. Voluptatum voluptatem, iusto
+                    aliquam repellendus optio molestias voluptates veritatis.
+                    Ratione atque molestias accusamus voluptatibus, eum cum,
+                    corrupti dicta rerum nesciunt pariatur debitis voluptatum
+                    dignissimos distinctio perspiciatis animi suscipit. Saepe
+                    nulla quis illum, eius aut, unde deleniti facere iure
+                    possimus dolores ducimus sint debitis quibusdam rerum
+                    accusamus velit dolor impedit nobis officiis corrupti
+                    quisquam modi laboriosam fugit? Molestiae quas ipsam esse
+                    repudiandae adipisci soluta, eum ea odio quos numquam?
+                    Officia quasi similique quae fugiat accusantium porro!
+                    Nostrum, similique. Quisquam velit placeat, cumque sequi
+                    tempore debitis ipsam voluptates, recusandae itaque sapiente
+                    eligendi totam nemo cupiditate nostrum? Nam neque dolores
+                    assumenda magnam doloremque non similique, laborum autem,
+                    porro ullam beatae qui?
+                </p>
+            </section>
+            <section className='flex flex-col gap-4'>
+                <SectionTitle size='text-3xl' text='How do we protect info?' />
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Eveniet, tempora corporis consequatur eaque excepturi, iure
+                    ipsa dolores ullam dolor facilis dignissimos molestiae quos
+                    sequi corrupti unde velit numquam dolore cum est officiis,
+                    ea fugiat! Qui perspiciatis veritatis possimus doloremque,
+                    mollitia corporis tenetur. Voluptatum voluptatem, iusto
+                    aliquam repellendus optio molestias voluptates veritatis.
+                    Ratione atque molestias accusamus voluptatibus, eum cum,
+                    corrupti dicta rerum nesciunt pariatur debitis voluptatum
+                    dignissimos distinctio perspiciatis animi suscipit. Saepe
+                    nulla quis illum, eius aut, unde deleniti facere iure
+                    possimus dolores ducimus sint debitis quibusdam rerum
+                    accusamus velit dolor impedit nobis officiis corrupti
+                    quisquam modi laboriosam fugit? Molestiae quas ipsam esse
+                    repudiandae adipisci soluta, eum ea odio quos numquam?
+                    Officia quasi similique quae fugiat accusantium porro!
+                    Nostrum, similique. Quisquam velit placeat, cumque sequi
+                    tempore debitis ipsam voluptates, recusandae itaque sapiente
+                    eligendi totam nemo cupiditate nostrum? Nam neque dolores
+                    assumenda magnam doloremque non similique, laborum autem,
+                    porro ullam beatae qui?
+                </p>
+            </section>
+            <section className='flex flex-col gap-4'>
+                <SectionTitle size='text-3xl' text='How can you contact us?' />
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Similique cumque delectus ratione voluptates, aperiam
+                    consequuntur? Culpa dolor dolorum possimus fugit beatae quam
+                    optio nesciunt? Atque aspernatur repudiandae ratione dolorem
+                    quasi ipsa illum perferendis ea similique vel maiores
+                    impedit esse, nisi iure nemo sint? Dolores minima earum
+                    placeat assumenda autem itaque?
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Eligendi non ex ipsam at tempora excepturi provident illum!
+                    Quo numquam facilis, animi dolorem provident magni, dolorum
+                    voluptates quas ut perferendis ipsa?
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Officiis atque ex consectetur eum maiores, accusamus
+                    obcaecati sequi odio dicta rerum?
+                </p>
+            </section>
+        </div>
+    );
+};
+export default TermsOfUse;
