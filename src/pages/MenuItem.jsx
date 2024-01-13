fd450520d514fa23a8fc5ac7f7fcb69194fdd35d
Chinatu Lucia Eke <chinatuluciaeke@gmail.com>
2024-01-13 19:33:52 +0100

    complete menu item page

--- src/pages/MenuItem.jsx
+++ src/pages/MenuItem.jsx
@@ -30,51 +30,75 @@ const MenuItem = () => {
     ];
 
     return (
-        <div className='flex flex-col px-4 lg:px-20 pt-8'>
-            <h1 className='text-2xl font-semibold'>{title}</h1>
-            <div className='flex gap-1 mt-4 bg-gray-200 w-max px-4 py-2'>
-                {breadcrumbs.map((breadcrumb, index) => {
-                    return <p key={breadcrumb} className="capitalize text-sm">{breadcrumb}{" "}{index < breadcrumbs.length - 1 ? "|" : ""} </p>;
-                })}
-            </div>
-            <img src={image} alt={title} className='w-96 object-contain' />
-            <div>
-                <h2 className="text-xl font-semibold mb-4">Nutritional Information</h2>
-                <div className="flex gap-4">
-                    {nutritionArray.map((nutrition) => {
-                        return (
-                            <div key={nutrition.name} className="flex gap-1 items-center justify-center rounded-sm bg-zinc-950 text-slate-50 w-24 py-1">
-                                <span>{nutrition.value}</span>
-                                <span>{nutrition.name}</span>
-                            </div>
-                        );
-                    })}
-                </div>
-                <table className="mt-4">
-                    <thead>
-                        <tr>
-                            <th>Nutrient</th>
-                            <th>Amount</th>
-                            <th>% of daily needs</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {nutrients.map((nutrient, index) => {
-                            const { name, amount, unit, percentOfDailyNeeds } =
-                                nutrient;
+        <div className=' px-4 lg:px-20 pt-8'>
+            <h1 className='text-3xl text-center font-semibold mb-6'>{title}</h1>
+            <div className='flex items-center justify-evenly gap-8'>
+                <div className='flex flex-col items-center'>
+                    <div className='flex gap-1 mt-4 bg-slate-100 w-max px-4 py-2'>
+                        {breadcrumbs.map((breadcrumb, index) => {
                             return (
-                                <tr key={index}>
-                                    <td>{name}</td>
-                                    <td>
-                                        {amount}
-                                        {unit}
-                                    </td>
-                                    <td>{percentOfDailyNeeds}</td>
-                                </tr>
+                                <p
+                                    key={breadcrumb}
+                                    className='capitalize text-sm'
+                                >
+                                    {breadcrumb}{" "}
+                                    {index < breadcrumbs.length - 1 ? "|" : ""}{" "}
+                                </p>
                             );
                         })}
-                    </tbody>
-                </table>
+                    </div>
+                    <img
+                        src={image}
+                        alt={title}
+                        className='w-96 object-contain'
+                    />
+                    <div className='flex gap-4'>
+                        {nutritionArray.map((nutrition) => {
+                            return (
+                                <div
+                                    key={nutrition.name}
+                                    className='flex gap-1 items-center justify-center rounded-sm bg-zinc-950 text-slate-50 w-24 py-1'
+                                >
+                                    <span>{nutrition.value}</span>
+                                    <span>{nutrition.name}</span>
+                                </div>
+                            );
+                        })}
+                    </div>
+                </div>
+                <div>
+                    <h2 className='text-xl font-semibold mb-4'>
+                        Nutritional Information
+                    </h2>
+                    <table className='mt-4 table-auto border-collapse border border-zinc-950'>
+                        <thead>
+                            <tr>
+                                <th>Nutrient</th>
+                                <th>Amount</th>
+                                <th>% of daily needs</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {nutrients.map((nutrient, index) => {
+                                const {
+                                    name,
+                                    amount,
+                                    unit,
+                                    percentOfDailyNeeds,
+                                } = nutrient;
+                                return (
+                                    <tr key={index}>
+                                        <td>{name}</td>
+                                        <td>
+                                            {amount} {unit}
+                                        </td>
+                                        <td>{percentOfDailyNeeds}</td>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     );
