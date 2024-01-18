5bdf5d1791722fd9c16e9b61b19af46b4cd32ac2
Chinatu Lucia Eke <chinatuluciaeke@gmail.com>
2024-01-18 18:42:31 +0100

    prevent data refetch on window focus

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,8 @@ import { loader as WinesLoader } from "./pages/Wines";
 const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
-            staleTime: 1000 * 60 * 5,
+        staleTime: 1000 * 60 * 5,
+          refetchOnWindowFocus: false,
         },
     },
 });
